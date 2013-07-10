bdbc2ca21406f6372ac3017a42836fed141b14d7
naysayer <joshuagregoryshuster@gmail.com>
2013-07-09 21:24:54 -0700

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Joshua Shuster on 7/9/13.
+//  Copyright (c) 2013 josh_matchismo_stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
